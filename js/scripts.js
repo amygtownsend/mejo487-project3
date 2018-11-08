5aef2fa8493cfe0ec5db54ea1c0b5cd3a46c61c6
Amy Townsend <amygtownsend@gmail.com>
2018-11-08 03:45:51 -0500

    make responsive for mobile, hide header when scroll down

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 $(document).ready(function (){
   console.log('scripts loaded');
 
-  var url = './js/bicycle-crash-data-chapel-hill-region.json';
-  var crashes = '';
-
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: url,
-    data: crashes,
-    async: true,
-    success: function(crashes){
-      console.log(crashes);
-      // Charts and table will go here
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-80px";
     }
+    prevScrollpos = currentScrollPos;
+  }
 
-  });
+  // var url = './js/bicycle-crash-data-chapel-hill-region.json';
+  // var crashes = '';
+  //
+  // $.ajax({
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   url: url,
+  //   data: crashes,
+  //   async: true,
+  //   success: function(crashes){
+  //     console.log(crashes);
+  //     // Charts and table will go here
+  //   }
+  //
+  // });
 });
