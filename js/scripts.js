a15809d3c66f2f86e88a3c7a6be1b30b7259396a
Amy Townsend <amygtownsend@gmail.com>
2019-02-26 11:55:48 -0500

    Remove data table JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
   console.log('scripts loaded');
 
   var url = './js/crashes-age-city.json';
@@ -10,34 +10,34 @@ $(document).ready(function (){
     url: url,
     data: crashes,
     async: true,
-    success: function(crashes){
+    success: function (crashes) {
       console.log(crashes);
       // Chart 1
       var chart = new Taucharts.Chart({
-          data: _(crashes)
-            .chain()
-            .reduce(function (memo, row) {
-        	    memo[row.BikeAgeGrp] = memo[row.BikeAgeGrp] || {
-            	   bikerAgeGroup: row.BikeAgeGrp,
-            	   crashes: 0
-        	    };
-          	  memo[row.BikeAgeGrp].crashes += 1;
-        	  return memo;
-            }, {})
-            .values()
-            .value(),
-          dimensions: {
-            'bikerAgeGroup': { type: 'category', scale: 'ordinal' },
-            'crashes': { type: 'measure', scale: 'linear' },
-          },
-          type: 'bar',
-          x: 'bikerAgeGroup',
-          y: 'crashes',
-          plugins: [
-            Taucharts.api.plugins.get('tooltip')({
-              fields: ['bikerAgeGroup', 'crashes']
-            })
-          ]
+        data: _(crashes)
+          .chain()
+          .reduce(function (memo, row) {
+            memo[row.BikeAgeGrp] = memo[row.BikeAgeGrp] || {
+              bikerAgeGroup: row.BikeAgeGrp,
+              crashes: 0
+            };
+            memo[row.BikeAgeGrp].crashes += 1;
+            return memo;
+          }, {})
+          .values()
+          .value(),
+        dimensions: {
+          'bikerAgeGroup': { type: 'category', scale: 'ordinal' },
+          'crashes': { type: 'measure', scale: 'linear' },
+        },
+        type: 'bar',
+        x: 'bikerAgeGroup',
+        y: 'crashes',
+        plugins: [
+          Taucharts.api.plugins.get('tooltip')({
+            fields: ['bikerAgeGroup', 'crashes']
+          })
+        ]
       });
       chart.renderTo('#chart1');
     }
@@ -49,63 +49,47 @@ $(document).ready(function (){
     url: url,
     data: crashes,
     async: true,
-    success: function(crashes){
+    success: function (crashes) {
       // Chart 2
       var chart = new Taucharts.Chart({
-          data: _(crashes)
-            .chain()
-            .reduce(function (memo, row) {
-              var k = row.BikeAgeGrp + row.City;
-        	    memo[k] = memo[k] || {
-                 bikerAgeGroup: row.BikeAgeGrp,
-                 city: row.City,
-            	   crashes: 0
-        	    };
-          	  memo[k].crashes += 1;
-        	  return memo;
-            }, {})
-            .values()
-            .value(),
-          dimensions: {
-            'bikerAgeGroup': { type: 'category', scale: 'ordinal' },
-            'crashes': { type: 'measure', scale: 'linear' },
-            'city' : { type: 'category', scale: 'ordinal' }
-          },
-          type: 'bar',
-          x: 'bikerAgeGroup',
-          y: 'crashes',
-          color: 'city',
-          plugins: [
-            Taucharts.api.plugins.get('tooltip')({
-              fields: ['bikerAgeGroup', 'crashes', 'city']
-            }),
-            Taucharts.api.plugins.get('legend')()
-          ]
+        data: _(crashes)
+          .chain()
+          .reduce(function (memo, row) {
+            var k = row.BikeAgeGrp + row.City;
+            memo[k] = memo[k] || {
+              bikerAgeGroup: row.BikeAgeGrp,
+              city: row.City,
+              crashes: 0
+            };
+            memo[k].crashes += 1;
+            return memo;
+          }, {})
+          .values()
+          .value(),
+        dimensions: {
+          'bikerAgeGroup': { type: 'category', scale: 'ordinal' },
+          'crashes': { type: 'measure', scale: 'linear' },
+          'city': { type: 'category', scale: 'ordinal' }
+        },
+        type: 'bar',
+        x: 'bikerAgeGroup',
+        y: 'crashes',
+        color: 'city',
+        plugins: [
+          Taucharts.api.plugins.get('tooltip')({
+            fields: ['bikerAgeGroup', 'crashes', 'city']
+          }),
+          Taucharts.api.plugins.get('legend')()
+        ]
       });
       chart.renderTo('#chart2');
     }
   });
-
-  $('#table').DataTable( {
-    ajax: {
-        url: './js/crashes-data.json',
-        dataSrc: ''
-    },
-    columns: [
-        { data: 'CrashYear' },
-        { data: 'City' },
-        { data: 'CrashSevr' },
-        { data: 'CrashType' },
-        { data: 'SpeedLimit' },
-        { data: 'AmbulanceR' },
-        { data: 'HitRun' }
-    ]
-  } );
 });
 
 // Sticky header disappears on scroll
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
