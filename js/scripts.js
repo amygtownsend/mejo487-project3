60d0b9cdc9b0ff27a59423453d1479636b771227
Amy Townsend <amygtownsend@gmail.com>
2019-02-26 22:36:24 -0500

    Update disappearing header js

--- js/scripts.js
+++ js/scripts.js
@@ -92,7 +92,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   console.log(currentScrollPos);
-  if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else if (currentScrollPos <= 0) {
     document.getElementById("header").style.top = "0";
   } else {
     document.getElementById("header").style.top = "-70px";
